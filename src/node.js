e770bfe3eaa113a325987ddd3740b7e9b3040f76
airKing05 <anil050797@gmail.com>
2023-10-08 17:45:46 +0530

    line box color change

--- src/node.js
+++ src/node.js
@@ -21,7 +21,7 @@ export default function () {
         .attr('height', 350)
         .attr('x', 100)
         .attr('y', 100)
-        .attr('fill', 'green')
+        .attr('fill', 'orange')
         .attr('stroke-width', 50)
       selection.append('text')
         .attr('class', 'node-value')
